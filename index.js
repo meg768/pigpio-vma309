86d18df6efefc520851848ad14731c6ed09fa609
meg768 <meg768@users.noreply.github.com>
2019-09-20 20:14:21 +0200

    -

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ module.exports = class SoundSensor extends Events {
 
 		super();
 
-        var {pin, event = 'alert', debug, delay = 100} = options;
+        var {pin, event = 'alert', debug, timeout = 100} = options;
 
 		if (pin == undefined)
 			throw new Error('Must supply a pin number.');
 
         if (typeof debug !== 'function')
-    		debug = function(){};
+    		debug = () => {};
 
-        debug('Construct options:', {pin:pin, event:event, debug:debug, delay:delay});
+        debug('Construct options:', {pin:pin, event:event, debug:debug, timeout:timeout});
 
         var gpio = new Gpio(pin, {mode: Gpio.INPUT, alert:true});
 
-		var timeout = null;
+		var timer = null;
 		var timestamp = null;
 
 		gpio.on('alert', (level, tick) => {
@@ -28,26 +28,26 @@ module.exports = class SoundSensor extends Events {
 
 				debug('GPIO alert: Level: %d, timestamp: %d', level, tick);
 
-				if (timeout != null) {
-					clearTimeout(timeout);
-					timeout = null;
+				if (timer != null) {
+					clearTimeout(timer);
+					timer = null;
 				}
 
 				if (timestamp == null) {
 					timestamp = tick;
 				}
 
-				timeout = setTimeout(() => {
+				timer = setTimeout(() => {
 					var duration = ((tick >> 0) - (timestamp >> 0)) / 1000;
 
 					debug('Sound duration: %d ms', duration);
 					this.emit(event, duration);
 
-					clearTimeout(timeout);
-					timeout = null;
+					clearTimeout(timer);
+					timer = null;
 					timestamp = null;
 
-				}, delay);
+				}, timeout);
 			}
 
 		});
